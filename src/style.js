35eed2c2bb63a44c4fabb705771032609196884a
Jeferson Beiertorf <jefersonbeiertorf@gmail.com>
2022-07-24 16:37:28 -0300

    mudanca header

--- src/style.js
+++ src/style.js
@@ -50,7 +50,7 @@ export const AreaCarrinho = styled.aside`
 `
 
 export const Header = styled.header`
-background-color: orange;
+background-color: #000005;
 width:100%;
 height: 6vw;
 display:flex;
